88de4a992ca1ee1012aee75d2e7a4e1949873c03
Sachiththa Bandaranayake <sacume3@gmail.com>
2024-02-22 16:19:37 +0100

    Temp setup  file

--- setup.py
+++ setup.py
@@ -1,2 +1,16 @@
-from setuptools import setup
-setup()
+import sys
+
+from setuptools import setup, find_packages
+setup(
+    name="sast",
+    version="0.0.1",
+    packages=find_packages(exclude=['tests*']),
+    install_requires=['asyncio', 'PyYAML'],
+    package_data = {
+        'sast': ['*.yaml'],
+        'sast.codeql': ['*yaml'],
+        'sast.codeql.codeql_v2_13_1' :['*yaml'],
+        'sast.codeql.codeql_v2_9_2' :['*yaml'],
+    }
+)
+
