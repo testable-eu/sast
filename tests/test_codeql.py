394cc73f5d5e8deb38968e55e419a6ac7f88cdc5
Sachiththa Bandaranayake <sacume3@gmail.com>
2024-02-23 11:01:30 +0100

    Update pytest.ini file

--- tests/test_codeql.py
+++ tests/test_codeql.py
@@ -2,13 +2,13 @@ import pytest
 from pathlib import Path
 from sast.codeql.codeql_v2_9_2.codeql import CodeQL_v_2_9_2
 
-test_resources_path = Path(__file__).resolve() / 'resources'
+from tests.config import TEST_RESOURCES_DIR
 
 
 class TestCodeQL:
 
     def test_inspector(self):
-        sarif_file = test_resources_path / "TPF_JS_codeql_2_9_2_1_instance_1_unset_element_array.sarif"
+        sarif_file = TEST_RESOURCES_DIR / "TPF_JS_codeql_2_9_2_1_instance_1_unset_element_array.sarif"
         language: str = "JS"
         inspection = CodeQL_v_2_9_2().inspector(sarif_file, language)
         assert inspection[0]["type"] == "xss"
