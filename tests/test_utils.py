4173ecf8470e1f2bf368d5e0ec4e1a102cef78d8
Sachiththa Bandaranayake <sacume3@gmail.com>
2024-02-23 10:42:08 +0100

    Implement unit test for utils

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,7 +1,20 @@
 import os
-from sast.utils import is_windows, get_exception_message, build_timestamp_language_name, zipdir
+from sast.utils import is_windows, get_exception_message, build_timestamp_language_name, zipdir, \
+    load_sast_specific_config, filter_sast_tools, get_class_from_str
 from datetime import datetime
 import zipfile
+from unittest.mock import patch, call
+import pytest
+from sast.exceptions import InvalidSastTool, InvalidSastTools
+import sast.config
+
+
+@pytest.fixture
+def sast_tools():
+    return [
+        {"name": "dummyTool1", "version": "1.0.0"},
+        {"name": "dummyTool2", "version": "2.0.0"}
+    ]
 
 
 class TestUtils:
@@ -26,6 +39,73 @@ class TestUtils:
         zipdir('TestDir', zip_file)
         assert (tmp_path / 'test.zip').is_file()
 
+    @patch('sast.utils.load_yaml')
+    def test_load_sast_specific_config(self, mock_load_yaml, sast_tools):
+        tool_name = sast_tools[0]['name']
+        tool_version = sast_tools[0]['version']
 
-    def test_load_sast_specific_config(self, monkeypatch):
-        pass
+        mock_load_yaml.side_effect = [
+            {"tools": {tool_name: {"version": {tool_version: {"config": "dummyTool/config.yaml"}}}}},
+            {"name": tool_name,
+             "version": tool_version,
+             "tool_interface": "sast.dummyTool1.dummyTool1",
+             "supported_languages": ["JAVA", "PHP", "JS"]
+             }
+        ]
+
+        result = load_sast_specific_config(tool_name, tool_version)
+
+        assert result == {"name": tool_name,
+                          "version": tool_version,
+                          "tool_interface": "sast.dummyTool1.dummyTool1",
+                          "supported_languages": ["JAVA", "PHP", "JS"]
+                          }
+        mock_load_yaml.assert_has_calls(
+            [call(sast.config.SAST_CONFIG_FILE), call(sast.config.ROOT / "dummyTool/config.yaml")])
+
+    @patch('sast.utils.load_yaml')
+    def test_load_sast_specific_config_fail_when_tool_not_found(self, mock_load_yaml):
+        tool_name = "dummyTool"
+        tool_version = "1.0.0"
+        mock_load_yaml.side_effect = InvalidSastTool("Invalid SAST Tool")
+        with pytest.raises(InvalidSastTool):
+            load_sast_specific_config(tool_name, tool_version)
+
+    @patch('sast.utils.load_sast_specific_config')
+    def test_filter_sast_tools_success(self, mock_load_sast_config, sast_tools):
+        # Arrange
+        input_tools = sast_tools
+        language = 'JAVA'
+        mock_load_sast_config.return_value = {'supported_languages': ['JAVA', 'PHP']}
+
+        # Act
+        result = filter_sast_tools(input_tools, language)
+
+        # Assert
+        assert result == input_tools
+        mock_load_sast_config.assert_called_with('dummyTool2', '2.0.0')
+
+    @patch('sast.utils.load_sast_specific_config')
+    def test_filter_sast_tools_no_tools_found(self, mock_load_sast_config, sast_tools):
+        # Arrange
+        input_tools = sast_tools
+        language = 'Ruby'
+        mock_load_sast_config.return_value = {'supported_languages': ['JAVA', 'PHP']}
+
+        # Act and Assert
+        InvalidSastTool("Invalid SAST Tool")
+        with pytest.raises(InvalidSastTools):
+            filter_sast_tools(input_tools, language)
+
+    def test_get_class_from_str_failure(self):
+        class_str = "non.existing.module.ClassName"
+
+        with pytest.raises(ImportError):
+            get_class_from_str(class_str)
+
+    def test_get_class_from_str_success(self):
+        class_str = "os.path.abspath"
+
+        result = get_class_from_str(class_str)
+
+        assert result == os.path.abspath
